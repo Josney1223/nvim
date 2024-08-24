e58b3693ce5b153e540aab6d318096b5b696934e
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-24 14:30:34 -0300

    updated from packer to lazy

--- lua/lessa/packer.lua
+++ lua/lessa/packer.lua
@@ -1,32 +1,48 @@
-vim.cmd [[packadd packer.nvim]]
+require('lazy').setup({
+    {
+        'nvim-telescope/telescope.nvim', version= '0.1.8',
+        -- or                            , branch = '0.1.x',
+        dependencies = { {'nvim-lua/plenary.nvim'} }
+    },
+    {
+        'rose-pine/nvim',
+        name = 'rose-pine',
+        priority = 1000,
+        lazy = false,
+        config = function()
+            vim.cmd([[colorscheme rose-pine]])
+        end
+    },
+    "mbbill/undotree",
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
+    },
+    "nvim-lua/plenary.nvim", -- don't forget to add this one if you don't have it yet!
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { {"nvim-lua/plenary.nvim"} }
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x'
+    },
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig"
+    },
+    'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function ()
+            require("toggleterm").setup()
+        end
+    },
+})
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
 
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  -- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }}
-
-  use({
-	  'rose-pine/nvim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
-  use "mbbill/undotree"
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
-  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-  use {
-	  "ThePrimeagen/harpoon",
-	  branch = "harpoon2",
-	  requires = { {"nvim-lua/plenary.nvim"} }
-  }
-  use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-  use {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim","neovim/nvim-lspconfig",}
-  use({'neovim/nvim-lspconfig'})
-  use({'hrsh7th/nvim-cmp'})
-  use({'hrsh7th/cmp-nvim-lsp'})
-end)
