a9eb90f67c5c87275ccbc5cb2a3d54ace88b35b1
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-24 15:37:23 -0300

    exit terminal

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -1,1 +1,6 @@
-vim.keymap.set('n', '<C-t>', "<cmd>ToggleTerm direction=vertical size=60<CR>")
+local exitTerm = function ()
+  vim.cmd(":ToggleTerm");
+end
+
+vim.keymap.set('n', "<C-t>", "<cmd>ToggleTerm direction=vertical size=100<CR>")
+vim.keymap.set('t', '<esc><esc>', exitTerm)
