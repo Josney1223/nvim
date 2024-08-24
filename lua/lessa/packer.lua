b098884dd474644ccd3550324fddf962e90d45f0
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-24 10:45:59 -0300

    Harpooon

--- lua/lessa/packer.lua
+++ lua/lessa/packer.lua
@@ -18,4 +18,10 @@ return require('packer').startup(function(use)
   })
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+  use {
+	  "ThePrimeagen/harpoon",
+	  branch = "harpoon2",
+	  requires = { {"nvim-lua/plenary.nvim"} }
+  }
 end)
