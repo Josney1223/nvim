e88d1f80a682e1d72621cf74af427261b41dddc0
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-26 15:32:08 -0300

    change tab to 4 spaces

--- lua/lessa/set.lua
+++ lua/lessa/set.lua
@@ -1,16 +1,16 @@
 local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
 if BinaryFormat == "so" then
-  vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 elseif BinaryFormat == "dll" then
-  vim.opt.undodir = os.getenv("HOMEPATH") .. "/.nvim/undodir"
+    vim.opt.undodir = os.getenv("HOMEPATH") .. "/.nvim/undodir"
 end
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.swapfile = false
