fe9e57fa0b344460c1e9b9730bb40308f98e25f8
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-26 13:21:01 -0300

    rm rust_analyzer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -46,7 +46,6 @@ require("mason").setup()
 require("mason-lspconfig").setup {
     ensure_installed = {
         "lua_ls",
-        "rust_analyzer",
         "gopls",
         "tsserver",
         "eslint",
