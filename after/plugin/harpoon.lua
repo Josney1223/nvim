31338e3bb97ded9ac0d9bbbb9352908edecdfb83
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-08-26 11:05:50 -0300

    next and previous harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,3 +6,6 @@ harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
