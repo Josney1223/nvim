789a8f2ee273d4aaf85de4474aa176f8cc10a04a
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2024-09-30 13:51:27 -0300

    change Flog for gitgraph

--- lua/mynvim/packer.lua
+++ lua/mynvim/packer.lua
@@ -52,12 +52,35 @@ require('lazy').setup({
     },
     'tpope/vim-fugitive',
     'folke/todo-comments.nvim',
+    -- FIX: Import muito ruim
     {
-        "rbong/vim-flog",
-        lazy = true,
-        cmd = { "Flog", "Flogsplit", "Floggit" },
-        dependencies = {
-            "tpope/vim-fugitive",
+        'isakbm/gitgraph.nvim',
+        opts = {
+            symbols = {
+                merge_commit = 'M',
+                commit = '*',
+            },
+            format = {
+                timestamp = '%H:%M:%S %d-%m-%Y',
+                fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+            },
+            hooks = {
+                on_select_commit = function(commit)
+                    print('selected commit:', commit.hash)
+                end,
+                on_select_range_commit = function(from, to)
+                    print('selected range:', from.hash, to.hash)
+                end,
+            },
         },
-    },
+        keys = {
+            {
+                "<leader>gg",
+                function()
+                    require('gitgraph').draw({}, { all = true, max_count = 5000 })
+                end,
+                desc = "GitGraph - Draw",
+            },
+        },
+    }
 })
