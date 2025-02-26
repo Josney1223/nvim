59f25aa0f5f0da7df9fe50d8133adb8cd0aee80c
Leonardo Lessa <leolopes00@hotmail.com>
2025-02-26 16:26:05 -0300

    fix same kaymap 2 functions

--- lua/mynvim/remap.lua
+++ lua/mynvim/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>ys", '"+y')
+vim.keymap.set("v", "<leader>ps", '"+p')
+vim.keymap.set("n", "<leader>ys", '"+y')
+vim.keymap.set("n", "<leader>ps", '"+p')
