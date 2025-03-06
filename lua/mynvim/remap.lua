4eac614401abcaae8982931c5b1686308b6555f4
Leonardo Lessa <leolopes00@hotmail.com>
2025-03-06 11:48:01 -0300

    new remap system buffer

--- lua/mynvim/remap.lua
+++ lua/mynvim/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("v", "<leader>ys", '"+y')
-vim.keymap.set("v", "<leader>ps", '"+p')
-vim.keymap.set("n", "<leader>ys", '"+y')
-vim.keymap.set("n", "<leader>ps", '"+p')
+vim.keymap.set("v", "<leader>yy", '"+y')
+vim.keymap.set("v", "<leader>py", '"+p')
+vim.keymap.set("n", "<leader>yy", '"+y')
+vim.keymap.set("n", "<leader>py", '"+p')
