b7ffecdff9291dc0c5f2d3583e1d0b1f6a1b57f3
Leonardo Lessa <leolopes00@hotmail.com>
2025-03-08 21:50:45 -0300

    remap delete to void register

--- lua/mynvim/remap.lua
+++ lua/mynvim/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>pp", "\"_dP")
 vim.keymap.set("v", "<leader>yy", '"+y')
 vim.keymap.set("v", "<leader>py", '"+p')
 vim.keymap.set("n", "<leader>yy", '"+y')
