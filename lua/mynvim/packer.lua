02510fb9d92e0f13dbef21e42154a7343e2fcd6c
Leonardo Lessa <leolopes00@hotmail.com>
2025-04-04 17:34:42 -0300

    update packages

--- lua/mynvim/packer.lua
+++ lua/mynvim/packer.lua
@@ -8,32 +8,7 @@ require('lazy').setup({
         dependencies = { { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' } }
     },
     {
-        'EdenEast/nightfox.nvim',
-        priority = 1000,
-        lazy = false
-    },
-    {
         'rose-pine/neovim',
-        priority = 1000,
-        lazy = false
-    },
-    {
-        'catppuccin/nvim',
-        priority = 1000,
-        lazy = false
-    },
-    {
-        'neanias/everforest-nvim',
-        priority = 1000,
-        lazy = false
-    },
-    {
-        'yorumicolors/yorumi.nvim',
-        priority = 1000,
-        lazy = false
-    },
-    {
-        'rebelot/kanagawa.nvim',
         priority = 1000,
         lazy = false
     },
@@ -56,11 +31,6 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
-    -- {
-    --     "akinsho/toggleterm.nvim",
-    --     version = "*",
-    --     config = true
-    -- },
     'tpope/vim-fugitive',
     'folke/todo-comments.nvim',
     'isakbm/gitgraph.nvim',
