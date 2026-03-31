469cabee124bbab8438d8ce8806c51d5ac653fe3
Leonardo Lessa <leolopes00@hotmail.com>
2026-03-31 11:52:18 -0300

    feat: removing writing, unused

--- lua/mynvim/init.lua
+++ lua/mynvim/init.lua
@@ -1,16 +1,6 @@
 print(_VERSION)
 print("NVIM Version " .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch)
 
-escolha = vim.fn.input("Está utilizando para código(1) ou escrita(2)?")
-print("\n")
-
-if escolha == "2" then
-    require("mynvim.remap")
-    require("mynvim.packer_writing")
-    require("mynvim.set")
-else
-    require("mynvim.remap")
-    require("mynvim.packer")
-    require("mynvim.set")
-end
-print("Bem vindo")
+require("mynvim.remap")
+require("mynvim.packer")
+require("mynvim.set")
